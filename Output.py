15fa86220c98c2e5e0a2429f428b1a9c54cc87a5
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-01 17:07:39 -0400

    Sigmoid function

--- Output.py
+++ Output.py
@@ -1,1 +1,9 @@
 # Neural-Network outputing training data
+
+import numpy as np 
+
+# sigmoid function
+def sigmoid(x,dirv=False):
+    if (dirv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
