b6f18c8b0e178f86cfbdd92267efe732adc74967
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-01 18:24:51 -0400

    Input/output data

--- Output.py
+++ Output.py
@@ -7,3 +7,12 @@ def sigmoid(x,dirv=False):
     if (dirv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
+
+# input data
+x = np.array ([ [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1] ])
+
+# output data
+y = np.array ([[1,0,0,1]]).T
