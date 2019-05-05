adaaca700ec8bfb1c6bbdb0170bd13552ed964df
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-04 21:19:35 -0400

    New function, data

--- 2layers.py
+++ 2layers.py
@@ -7,4 +7,16 @@ def sigmoid(x):
     output: 1/(1+np.exp(-x))
     return output
 
+# output of sigmoid to dirv
+def sigmoid_to_dirv(output):
+    return output*(1-output)
+
+# input data
+x = np.array([ [0,1],
+               [0,1],
+               [1,0],
+               [1,0] ])
+
+# output data
+y = np.array ([[0,0,1,1]]).T
 
