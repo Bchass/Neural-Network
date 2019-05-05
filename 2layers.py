858eb1a0b47b7f8dcf2335feb9ee9eaf305c20ad
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-05 14:19:16 -0400

    Error and weights

--- 2layers.py
+++ 2layers.py
@@ -23,5 +23,28 @@ y = np.array ([[0,0,1,1]]).T
 # random calculations
 np.random.seed(1)
 
+# weights randomly
+weight1 = 2*np.random.random((2,1)) - 1
+
+for iter in range(10000):
+
+    # forward prop
+    layer0 = x
+    layer1 = sigmoid(np.dot(layer0,weight1))
+
+    # error
+    layer1e = layer1 - y
+
+    # calc how much was missed in slope with sigmoid
+    layer1d = layer1e * sigmoid_to_dirv(layer1)
+    weight1_dirv = np.dot(layer0.T,layer1d)
+
+    # update the weights
+    weight1 -= weight1_dirv
+
+print("Output after training:")
+print(layer1)
+
+
 
 
