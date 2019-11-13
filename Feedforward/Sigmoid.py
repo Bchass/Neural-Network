300ce791b7f619b9fe7c2042f00cad1248af0887
Brandon Chasser <brandonchasser22@gmail.com>
2019-11-13 14:39:49 -0500

    forward pass and sigmoid function

---
+++ Feedforward/Sigmoid.py
@@ -0,0 +1,14 @@
+import numpy as np
+class Sigmoid:
+    def _init_(self):
+        self.W = None #weights
+    self.B = None #biases
+
+# forward pass
+def perceptron(self, x):
+    #take weights and biases
+    return np.dot(x,self.w.T) + self.B 
+# sigmoid function
+def Sigmoid(self,x):
+    return 1.0/(1.0+np.exp(-x))
+
